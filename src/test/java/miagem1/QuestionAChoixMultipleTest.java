2d6dafe7e68e22ab8ed1f30d139251128661efd4
pouget <nicolaspouget@orange.fr>
2019-11-29 17:08:05 +0100

    ix #2 Tests sur QuestionAChoixMultiple

--- src/test/java/miagem1/QuestionAChoixMultipleTest.java
+++ src/test/java/miagem1/QuestionAChoixMultipleTest.java
@@ -31,6 +31,9 @@ public class QuestionAChoixMultipleTest {
 
         // then : l'énoncé est non null
         assertNotNull(resEnonce);
+
+        // and : l'énoncé est bien celui fourni à la construction
+        assertEquals("un énoncé", resEnonce);
     }
 
     @Test
@@ -40,13 +43,15 @@ public class QuestionAChoixMultipleTest {
         // and : on demande le calcule du score
         Float resScore = this.uneQuestion.getScoreForIndice(indiceEtudiant);
         // then : le score obtenu est de 100
-        assertEquals(new Float(100f), resScore);
+        assertEquals(new Float(50f), resScore);
         // when : un étudiant fourni l'indice correspondant à une mauvaise réponse
         indiceEtudiant = 3;
         // and : on demande le calcule du score
         resScore = this.uneQuestion.getScoreForIndice(indiceEtudiant);
         // then : le score obtenu est de 100
         assertEquals(new Float(0f), resScore);
+
+
     }
 
 }
